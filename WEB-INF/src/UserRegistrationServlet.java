ff5bb6de9b6c78f643485a3866bf590fde8d5656
“Jay” <j.nwanze@gmail.com>
2023-10-28 21:25:00 +0100

    Updating UserRegistrationServlet.java *restructure of try/catch statements and validatation logic through use of boolean variables *Updated user username request to be stored as all lowercase characters *Added validation for blank user inputs *Added validation for password/password confirmation that it must be minimum 8 characters and maximum 20.

--- WEB-INF/src/UserRegistrationServlet.java
+++ WEB-INF/src/UserRegistrationServlet.java
@@ -18,7 +18,7 @@ public class UserRegistrationServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         // Gets users input from https request and initialize to variables
-        String username = request.getParameter("username").toLowerCase();//
+        String username = request.getParameter("username").toLowerCase();
         String password = request.getParameter("password");
         String passConfirmation = request.getParameter("passwordConfirmation");
         int points = 100;// Default value of 100
@@ -44,87 +44,125 @@ public class UserRegistrationServlet extends HttpServlet {
         // Declaring/Initializing functionality to send reponse to user
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        boolean notBlankInput = false;
+        boolean passwordCorrectLength = false;
+        boolean passwordMatches = false;
 
-        try {
-            // Check if password is equal to password confirmation
-            if (password.equals(passConfirmation)) {
+        // Check if passed in user inputs is blank
+        if (!(username.isBlank()) && !(password.isBlank()) && !(passConfirmation.isBlank())) {
+            notBlankInput = true;
+            // Check if password/password confirmation length is within correct range
+            if (password.length() >= 8 && password.length() <= 20 && passConfirmation.length() >= 8
+                    && passConfirmation.length() <= 20) {
+                passwordCorrectLength = true;
+                // Check if password is equal to password confirmation
+                if (password.equals(passConfirmation)) {
+                    passwordMatches = true;
+                    // Create statement to check if username is taken
+                    Statement checkIfUsernameIsTaken = null;
+                    try {
+                        checkIfUsernameIsTaken = connection.createStatement();
 
-                // Create statement to check if username is taken
-                Statement checkIfUsernameIsTaken = null;
-                try {
-                    checkIfUsernameIsTaken = connection.createStatement();
+                    } catch (SQLException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
 
-                } catch (SQLException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
-
-                // Execute sql query to check if username is taken or not
-                ResultSet rs = null;
-                boolean usernameTaken = false;
-                try {
                     // Executes sql query to check if username already exists
-                    rs = checkIfUsernameIsTaken
-                            .executeQuery("SELECT username FROM users WHERE username = '" + username + "'");
-
-                    // If resultset is returned
-                    if (rs.next()) {
-                        // Get username as string
-                        String usernameDB = rs.getString(1);
-                        // If username already exist set boolean to true
-                        if (usernameDB != null && username.equalsIgnoreCase(usernameDB)) {
-
-                            usernameTaken = true;
+                    ResultSet rs = null;
+                    boolean usernameTaken = false;
+                    try {
+                        rs = checkIfUsernameIsTaken
+                                .executeQuery("SELECT username FROM users WHERE username = '" + username + "'");
+                    } catch (SQLException e1) {
+                        // TODO: handle exception
+                        e1.printStackTrace();
+                    }
+                    // Check to see if resultset is returned
+                    try {
+                        if (rs.next()) {
+                            // Get username as string
+                            String usernameDB = rs.getString(1);
+                            // If username already exists set boolean to true
+                            if (usernameDB != null && username.equalsIgnoreCase(usernameDB)) {
+                                usernameTaken = true;
+                            }
                         }
+
+                    } catch (SQLException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+                    try {
+                        // If username is not taken then execute query to create a new user within
+                        // database
+                        if (usernameTaken == false) {
+                            PreparedStatement createUser = connection.prepareStatement(
+                                    "INSERT into users "
+                                            + "(username, password, points)" + " VALUES (?, ?, ?)");
+                            // Pass in the values as paramaters into sql statement
+                            createUser.setString(1, username);
+                            createUser.setString(2, password);
+                            createUser.setInt(3, points);
+
+                            int rowsUpdated = createUser.executeUpdate();
+                            createUser.close();
+
+                            // If user was created - send reponse that user has been created sucessfully
+                            // /direct to login page
+                            if (rowsUpdated > 0) {
+                                out.println("<html><head><title>LoyaltyAppWebsite </title></head>"
+                                        + "<body> <h1> Welcome: " + username
+                                        + "<br>You have Been Succesfully Created as a User!</h1><h3>Now Redirecting You To The Login Page - Please Wait a Few Seconds or Press Button To Go To The Login Page...<br>"
+                                        + "<br><input type='button' value='Login Page' onclick=\"window.location.href='login.html'\"/></h3> <script>setTimeout(function () {window.location.href = 'login.html';}, 8000);</script></body></html>");
+
+                            } else {
+                                // if user user wasnt created sucessfully - send reponse/redirect back to
+                                // registration page
+                                out.println("<html><head><title>LoyaltyAppWebsite </title></head>"
+                                        + "<body> <h1>User Was Not Created Sucessfully - Please Try Register Again!</h1><h3>Now Redirecting You Back To The Registration Page - Please Wait a Few Seconds or Press Button To Return To The Registration Page...<br>"
+                                        + "<br><input type='button' value='Registration Page' onclick=\"window.location.href='index.html'\"/></h3> <script>setTimeout(function () {window.location.href = 'index.html';}, 8000);</script></body></html>");
+
+                            }
+                        }
+                        // If username already in use - send reponse/redirect back to registration page
+                        else {
+                            out.println("<html><head><title>LoyaltyAppWebsite </title></head>"
+                                    + "<body> <h1> The Username: " + username
+                                    + "<br>Is Already In Use - Please Try Register Again!</h1><h3>Now Redirecting You Back To The Registration Page - Please Wait a Few Seconds or Press Button To Return To The Registration Page...<br>"
+                                    + "<br><input type='button' value='Registration Page' onclick=\"window.location.href='index.html'\"/></h3> <script>setTimeout(function () {window.location.href = 'index.html';}, 8000);</script></body></html>");
+                        }
+                    } catch (SQLException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
                     }
 
-                } catch (SQLException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
-
-                // If username is not taken then execute query to create a new user within
-                // database
-                if (usernameTaken == false) {
-                    PreparedStatement createUser = connection.prepareStatement(
-                            "INSERT into users "
-                                    + "(username, password, points)" + " VALUES (?, ?, ?)");
-                    // Pass in the values as paramaters into sql statement
-                    createUser.setString(1, username);
-                    createUser.setString(2, password);
-                    createUser.setInt(3, points);
-
-                    int rowsUpdated = createUser.executeUpdate();
-                    createUser.close();
-
-                    // If user was created -send reponse that user has been created sucessfully
-                    if (rowsUpdated > 0) {
-                        out.println("<html><head><title>LoyaltyAppWebsite </title></head>"
-                                + "<body> <h1> Welcome: " + username
-                                + " , You have Been Succesfully Created as a User!</h1><h3>Now Redirecting You To The Login Page - Please Wait a Few Seconds or Press Button To Go To The Login Page...<br>"
-                                + "<br><input type='button' value='Login Page' onclick=\"window.location.href='login.html'\"/></h3> <script>setTimeout(function () {window.location.href = 'login.html';}, 8000);</script></body></html>");
-
-                    } else {
-                        // reponse - user wasnt created sucessfully try again
-                    }
-                }
-                // If username already in use - send reponse/redirect back to registration page
-                else {
-                    out.println("<html><head><title>LoyaltyAppWebsite </title></head>"
-                            + "<body> <h1> The Username: " + username
-                            + ", is Already In Use - Please Try Register Again!</h1><h3>Now Redirecting You Back To The Registration Page - Please Wait a Few Seconds or Press Button to Return to Registration Page...<br>"
-                            + "<br><input type='button' value='Registration Page' onclick=\"window.location.href='index.html'\"/></h3> <script>setTimeout(function () {window.location.href = 'index.html';}, 8000);</script></body></html>");
                 }
             }
-            // Else if password and password confirmation do not match - send reponse
-            else {
-                out.println("<html><head><title>LoyaltyAppWebsite </title></head>"
-                        + "<body> <h1> Your Password and Password Confirmation Don't Match - Please Try Register Again!</h1><h3>Now Redirecting You Back To The Registration Page - Please Wait a Few Seconds or Press Button to Return to Registration Page...<br>"
-                        + "<br><input type='button' value='Registration Page' onclick=\"window.location.href='index.html'\"/></h3> <script>setTimeout(function () {window.location.href = 'index.html';}, 8000);</script></body></html>");
-            }
 
-        } catch (SQLException e1) {
-            e1.printStackTrace();
+        }
+        // If user input field/fields were blank - send reponse/redirect back to
+        // registration page
+        if (notBlankInput == false) {
+            out.println("<html><head><title>LoyaltyAppWebsite </title></head>"
+                    + "<body> <h1>Blank Input Field/Fields  - Please Try Register Again!</h1><h3>Now Redirecting You Back To The Registration Page - Please Wait a Few Seconds or Press Button To Return To The Registration Page...<br>"
+                    + "<br><input type='button' value='Registration Page' onclick=\"window.location.href='index.html'\"/></h3> <script>setTimeout(function () {window.location.href = 'index.html';}, 8000);</script></body></html>");
+
+        }
+        // Else if password/password confirmation is not within correct length name -
+        // send reponse/redirect back to registration page
+        else if (passwordCorrectLength == false) {
+            out.println("<html><head><title>LoyaltyAppWebsite </title></head>"
+                    + "<body> <h1>Password/Password Confirmation Must Be Minimum 8 Characters/Maxmium 20 Characters<br>Please Try Register Again!</h1><h3>Now Redirecting You Back To The Registration Page - Please Wait a Few Seconds or Press Button To Return To The Registration Page...<br>"
+                    + "<br><input type='button' value='Registration Page' onclick=\"window.location.href='index.html'\"/></h3> <script>setTimeout(function () {window.location.href = 'index.html';}, 8000);</script></body></html>");
+
+        }
+        // Else if password and password confirmation do not match - send
+        // reponse/redirect back to registration page
+        else if (passwordMatches == false) {
+            out.println("<html><head><title>LoyaltyAppWebsite </title></head>"
+                    + "<body> <h1> Your Password and Password Confirmation Don't Match - Please Try Register Again!</h1><h3>Now Redirecting You Back To The Registration Page - Please Wait a Few Seconds or Press Button To Return To The Registration Page...<br>"
+                    + "<br><input type='button' value='Registration Page' onclick=\"window.location.href='index.html'\"/></h3> <script>setTimeout(function () {window.location.href = 'index.html';}, 8000);</script></body></html>");
         }
 
     }
